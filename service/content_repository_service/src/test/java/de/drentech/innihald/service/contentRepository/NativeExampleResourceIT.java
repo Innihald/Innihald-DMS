a5b66b90aab81dce4e5ac61a91fc12864a0d5f92
Marian Hahne <info@mariansievers.de>
2019-10-27 22:21:23 +0100

    Add bootstrap code for content_repository_service

---
+++ service/content_repository_service/src/test/java/de/drentech/innihald/service/contentRepository/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package de.drentech.innihald.service.contentRepository;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
