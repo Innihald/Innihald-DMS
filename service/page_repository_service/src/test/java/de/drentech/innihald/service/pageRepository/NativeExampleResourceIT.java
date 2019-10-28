b63c3a48eb489c8aedc2db9df6dc12c72089bde6
Marian Hahne <info@mariansievers.de>
2019-10-28 20:19:16 +0100

    Add page_repository_service

---
+++ service/page_repository_service/src/test/java/de/drentech/innihald/service/pageRepository/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package de.drentech.innihald.service.pageRepository;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
